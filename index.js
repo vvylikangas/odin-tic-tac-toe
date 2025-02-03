46768eb0d046d0c351d8500c4a03e7336ab157a9
Ville Ylikangas <villeylik@gmail.com>
2025-02-03 10:04:32 +0200

    Add gameboard iife

---
+++ index.js
@@ -0,0 +1,16 @@
+const GameBoard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i][j] = null;
+    }
+  }
+
+  const getBoard = () => board;
+
+  return { getBoard };
+})();
