9adb41af498981b1b1e7e08047a25c7c7ebb54bd
Ville Ylikangas <villeylik@gmail.com>
2025-02-03 10:13:19 +0200

    Add gamecontroller iife

--- index.js
+++ index.js
@@ -21,3 +21,17 @@ const Player = (name, symbol) => {
 
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
+
+const GameController = (function () {
+  let currentPlayer = player1;
+
+  const switchTurn = () => {
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  };
+
+  const getCurrentPlayer = () => currentPlayer;
+
+  return { switchTurn, getCurrentPlayer };
+})();
+
+console.log(GameController.getCurrentPlayer());
